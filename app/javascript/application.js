0a46f7d51ea2d7d2065a53d1d40b0e8dd1efe68b
Paul Bob <paul.ionut.bob@gmail.com>
2024-11-25 14:15:02 +0200

    demo

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,12 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import { Application } from "@hotwired/stimulus"
+import HwComboboxController from '@josefarias/hotwire_combobox'
+
+const application = Application.start()
+
+// Add the following two lines:
+application.register("hw-combobox", HwComboboxController)
+
+export { application }
